c83f9f032417819077c2815bb152ca3b732f26c7
SyedWaleedAli71 <syedwaleed887@gmail.com>
2025-11-20 07:59:00 +0500

    first-commit

---
+++ app.js
@@ -0,0 +1,50 @@
+// ARRAYS
+// Q.1
+// var studentName = [];
+// 
+// 
+// Q.2
+// var studentName = Array();
+
+// var city = "karachi";
+// var city1 = "Hyderabad";
+// var city2 = "Lahore";
+// var city3 = "Islamabad";
+
+// alert("Welcome to" +" "+city3);
+//  var cities = ["karachi", "lahore","Islamabad","hyderabad"];
+
+//  alert("Welcome To " + " " + cities[0])
+
+// 
+// Q.3
+//  var cities = ["karachi", "lahore","Islamabad","hyderabad"];
+// 
+// Q.4
+// var numBers = ["10","20","30","40","50"];
+// alert(numBers[0]);
+// alert(numBers[1]);
+// alert(numBers[2]);
+// 
+// Q.5
+// var booleanArray = [true, false, true, false];
+// 
+// Q.6
+// var mixedArray =["Waleed",23, true, "Ali",22, false];
+// 
+// Q.7
+// var qualification = ["SSC","HSC","BSC","BS","BCOM","MS","M.Phil.","PHD"];
+// console.log(qualification);
+// document.writeln(qualification);
+// 
+// Q.8
+// var students = ["Syed","Waleed","Ali"];
+
+// var scores = [320, 230, 480];
+// var totalMarks = 500;
+
+// document.writeln("Score of " + students[0] + " is " + scores[0] + ". Percentage: " + (scores[0] / totalMarks * 100) + "%<br>");
+// document.writeln("Score of " + students[1] + " is " + scores[1] + ". Percentage: " + (scores[1] / totalMarks * 100) + "%<br>");
+// document.writeln("Score of " + students[2] + " is " + scores[2] + ". Percentage: " + (scores[2] / totalMarks * 100) + "%<br>");
+// 
+// Q.9
